661803df5a6272b32f1e44c94da22d3e78250717
Jorgeley <jorgeley@gmail.com>
2016-01-11 14:41:32 -0200

    recorte de logo funcionando, cadastro do fornecedor ok

--- upload.php
+++ upload.php
@@ -1,22 +1,13 @@
 <?php
 if ($_POST){
-    echo "post";
-    if ($_POST['img']){
-        echo "recorta";
-        $img = new Img(
-            1, //qtd imgs
-            $_POST, //post of hidden coordenates
-            array($_POST['img']), //image address
-            $_POST['img'], //relative image path
-            '/var/www/html/nasuakz/img/logo/', //absolute image path
-            144, //weight cut
-            144, //height cut
-            $_POST['img'] //image identifier, Ex: $id.jpg (one image) OR $id/1.jpg, $id/2.jpg (many images)
-        );
-        $img->recorta();
+    if ($_POST['imgLogo']){
+        list($width, $height) = getimagesize($_POST['imgLogo']);
+        $image_p = imagecreatetruecolor(144, 144);
+        $image = imagecreatefromjpeg($_POST['imgLogo']);
+        imagecopyresampled($image_p, $image, 0, 0, $_POST['x0'], $_POST['y0'], 144, 144, $_POST['w0'], $_POST['h0']);
+        imagejpeg($image_p, $_POST['imgLogo'], 100);
     }
-}else if ($_FILES){
+}else if ($_FILES)
         echo copy($_FILES['arquivoLogo']['tmp_name'], 'img/logo/'.$_FILES['arquivoLogo']['name']);
-    }else echo "nada";
 
 ?>

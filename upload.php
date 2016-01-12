94c8b9e57d87b2a817fe7de9184d64f387dfd516
Jorgeley <jorgeley@gmail.com>
2016-01-12 17:02:33 -0200

    tamanho das imagens trocado 288x288

--- upload.php
+++ upload.php
@@ -2,9 +2,9 @@
 if ($_POST){
     if ($_POST['imgLogo']){
         list($width, $height) = getimagesize($_POST['imgLogo']);
-        $image_p = imagecreatetruecolor(144, 144);
+        $image_p = imagecreatetruecolor(288, 288);
         $image = imagecreatefromjpeg($_POST['imgLogo']);
-        imagecopyresampled($image_p, $image, 0, 0, $_POST['x0'], $_POST['y0'], 144, 144, $_POST['w0'], $_POST['h0']);
+        imagecopyresampled($image_p, $image, 0, 0, $_POST['x0'], $_POST['y0'], 288, 288, $_POST['w0'], $_POST['h0']);
         imagejpeg($image_p, $_POST['imgLogo'], 100);
     }
 }else if ($_FILES)

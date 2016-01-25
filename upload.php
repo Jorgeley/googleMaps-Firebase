10839004fd676d04c234bf0d073e04864dc2e2ef
Jorgeley <jorgeley@gmail.com>
2016-01-25 16:50:27 -0200

    Cadastro de Novo Produto

--- upload.php
+++ upload.php
@@ -1,13 +1,24 @@
 <?php
 if ($_POST){
-    if ($_POST['imgLogo']){
-        list($width, $height) = getimagesize($_POST['imgLogo']);
-        $image_p = imagecreatetruecolor(288, 288);
-        $image = imagecreatefromjpeg($_POST['imgLogo']);
-        imagecopyresampled($image_p, $image, 0, 0, $_POST['x0'], $_POST['y0'], 288, 288, $_POST['w0'], $_POST['h0']);
-        imagejpeg($image_p, strtolower(str_replace(" ", "", $_POST['imgLogo'])), 100);
+    if ($_POST['imgLogo'])
+        $img = $_POST['imgLogo'];
+    if ($_POST['imgProduto'])
+        $img = $_POST['imgProduto'];
+    list($width, $height) = getimagesize($img);
+    $image_p = imagecreatetruecolor(288, 288);
+    $image = imagecreatefromjpeg($img);
+    imagecopyresampled($image_p, $image, 0, 0, $_POST['x0'], $_POST['y0'], 288, 288, $_POST['w0'], $_POST['h0']);
+    imagejpeg($image_p, strtolower(str_replace(" ", "", $img)), 100);
+    
+}else if ($_FILES){
+    if ($_FILES['arquivoLogo']){
+        $file = $_FILES['arquivoLogo'];
+        $dir = "img/logo/";
     }
-}else if ($_FILES)
-        echo copy($_FILES['arquivoLogo']['tmp_name'], 'img/logo/'.$_FILES['arquivoLogo']['name']);
-
+    if ($_FILES['arquivoProduto']){
+        $file = $_FILES['arquivoProduto'];
+        $dir = "img/produtos/";
+    }
+    echo copy($file['tmp_name'], $dir.$file['name']);
+}
 ?>
